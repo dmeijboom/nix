5dda7dfe647ae6f5878f0f2fe1edba35af7ed1b5
Dillen Meijboom <dillen@brainhive.nl>
2025-09-10 23:05:32 +0200

    feat: remove lazygit in favor of neogit

--- config/git.nix
+++ config/git.nix
@@ -5,8 +5,6 @@
   ...
 }:
 {
-  home.shellAliases.s = "lazygit";
-
   programs = {
     git = {
       enable = true;
@@ -39,74 +37,6 @@
         ".direnv"
         ".zed/tasks.json"
       ];
-    };
-
-    lazygit.enable = true;
-    lazygit.settings = {
-      customCommands = [
-        {
-          key = "s";
-          context = "files";
-          command = "git stash -u -- {{.SelectedFile.Name | quote}}";
-        }
-        {
-          key = "B";
-          context = "files";
-          prompts = [
-            {
-              type = "menu";
-              title = "What kind of commit/branch?";
-              key = "BranchType";
-              options = [
-                {
-                  name = "feature";
-                  value = "feat";
-                }
-                {
-                  name = "hotfix";
-                  value = "fix";
-                }
-                {
-                  name = "refactor";
-                  value = "refactor";
-                }
-              ];
-            }
-            {
-              type = "input";
-              title = "Commit message?";
-              key = "Message";
-              initialValue = "";
-            }
-          ];
-          command = "git checkout -b {{ .Form.BranchType }}/$(echo {{ .Form.Message | quote }} | sed 's/[ \\(\\)@]/-/g' | tr '[:upper:]' '[:lower:]') && git commit -m '{{ .Form.BranchType }}: {{ .Form.Message }}'";
-        }
-      ];
-      promptToReturnFromSubprocess = false;
-      git.paging.pager = "delta --dark --paging=never";
-      git.autoFetch = false;
-      gui.statusPanelView = "allBranchesLog";
-      gui.authorColors = {
-        "*" = "#b7bdf8";
-      };
-      gui.showRandomTip = false;
-      gui.showCommandLog = false;
-      gui.showPanelJumps = false;
-      gui.screenMode = "half";
-      gui.theme = {
-        activeBorderColor = [
-          "#8aadf4"
-          "bold"
-        ];
-        inactiveBorderColor = [ "#a5adcb" ];
-        optionsTextColor = [ "#8aadf4" ];
-        selectedLineBgColor = [ "#363a4f" ];
-        cherryPickedCommitBgColor = [ "#494d64" ];
-        cherryPickedCommitFgColor = [ "#8aadf4" ];
-        unstagedChangesColor = [ "#ed8796" ];
-        defaultFgColor = [ "#cad3f5" ];
-        searchingActiveBorderColor = [ "#eed49f" ];
-      };
     };
   };
 }
