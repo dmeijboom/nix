a2bdb450c78e394dfebf3e56f33367c2c09a74e3
Dillen Meijboom <dillen@brainhive.nl>
2025-10-05 11:41:02 +0200

    feat: move to alacritty for performance

--- config/shell.nix
+++ config/shell.nix
@@ -27,7 +27,6 @@
       };
       envExtra = ''
         export FZF_CTRL_T_COMMAND=
-        export KONSOLE_VERSION="230804"
       '';
       initContent = ''
         update_status() {
@@ -54,18 +53,30 @@
     fzf.enable = true;
     fzf.enableZshIntegration = true;
 
-    rio.enable = true;
-    rio.settings = {
+    alacritty = {
+      enable = true;
       theme = "nord";
-      hide-mouse-cursor-when-typing = true;
-      renderer.performance = "High";
-      option-as-alt = "both";
-      fonts = {
-        family = "JetBrainsMono Nerd Font";
-      };
-      cursor = {
-        shape = "underline";
-        blinking = false;
+      settings = {
+        env.KONSOLE_VERSION = "230804";
+        general.live_config_reload = true;
+        mouse.hide_when_typing = true;
+        window = {
+          decorations = "None";
+          startup_mode = "Maximized";
+          option_as_alt = "OnlyLeft";
+          padding = { x = 8; y = 8; };
+        };
+        font = {
+          size = 14;
+          normal.family = "JetBrainsMono Nerd Font";
+          bold.family = "JetBrainsMono Nerd Font";
+          italic.family = "JetBrainsMono Nerd Font";
+          bold_italic.family = "JetBrainsMono Nerd Font";
+        };
+        cursor.style = {
+          shape = "Underline";
+          blinking = "Never";
+        };
       };
     };
 
