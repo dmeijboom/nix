a70e3e5c9f7a9482b2183ac5e214438d6c0b36c0
Dillen Meijboom <dillen@brainhive.nl>
2025-12-05 17:33:43 +0100

    feat: update yazi theme

--- config/shell/yazi.nix
+++ config/shell/yazi.nix
@@ -1,22 +1,54 @@
 {
   pkgs,
+  lib,
   ...
 }:
+let
+  plugins = [
+    "no-status"
+    "git"
+    "full-border"
+  ];
+
+  # Generate the attrset for programs.yazi.plugins
+  pluginAttrs = lib.listToAttrs (
+    map (name: {
+      inherit name;
+      value = pkgs.yaziPlugins.${name};
+    }) plugins
+  );
+
+  # Generate the initLua requires
+  pluginInits = lib.concatMapStringsSep "\n" (name: ''require("${name}"):setup()'') plugins;
+in
 {
   programs.yazi = {
     enable = true;
-    settings.mgr.show_symlink = false;
-    plugins = with pkgs.yaziPlugins; {
-      inherit nord full-border;
+    settings = {
+      mgr = {
+        show_symlink = false;
+        show_hidden = true;
+      };
+      plugin.prepend_fetchers = [
+        {
+          id = "git";
+          name = "*";
+          run = "git";
+        }
+        {
+          id = "git";
+          name = "*/";
+          run = "git";
+        }
+      ];
     };
-    flavors = { inherit (pkgs.yaziPlugins) nord; };
-    theme.flavor = {
-      light = "nord";
-      dark = "nord";
+    plugins = pluginAttrs;
+    initLua = pluginInits;
+    theme = {
+      mgr = {
+        border_style.fg = "#363b48";
+        hovered.bg = "#4e5668";
+      };
     };
-    initLua = ''
-      require("nord"):setup()
-      require("full-border"):setup()
-    '';
   };
 }
